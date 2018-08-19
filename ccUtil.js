f60c1f4ef1bda3664d2f90d6256b6e4153647e8e
jqg <185356122@qq.com>
2018-08-19 15:00:30 +0800

    added btc tx functions and removed unused files

--- ccUtil.js
+++ ccUtil.js
@@ -1,6 +1,11 @@
 'use strict'
 
+const coinSelect = require('coinselect')
+const feeRate = 55 // satoshis per byte
 
+var crypto = require('crypto')
+var secp256k1 = require('secp256k1')
+var createKeccakHash = require('keccak')
 const pu = require('promisefy-util');
 const BigNumber = require('bignumber.js');
 const wanUtil = require("wanchain-util");
@@ -43,6 +48,18 @@ const WebSocket = require('ws');
 const Web3 = require("web3");
 var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const FEE = 0.001
+const MAX_CONFIRM_BLKS = 10000000
+const MIN_CONFIRM_BLKS = 0
+const LOCK_BLK_NUMBER = 10
+
+const TX_EMPTY_SIZE = 4 + 1 + 1 + 4
+const TX_INPUT_BASE = 32 + 4 + 1 + 4
+const TX_INPUT_PUBKEYHASH = 107
+const TX_OUTPUT_BASE = 8 + 1
+const TX_OUTPUT_PUBKEYHASH = 25
+const network = bitcoin.networks.testnet
+var contractsMap = {}
+var XXMap = {}
 
 const Backend = {
     CreaterSockSenderByChain(ChainType) {
@@ -598,6 +615,388 @@ const Backend = {
         return btcHash;
     },
 
+  /// /////////////////////////////////////////////////btc functions///////////////////////////////////////////////////////
+
+  hashtimelockcontract (storemanHash160, redeemblocknum, destHash160, revokerHash160) {
+    let x = redeemblocknum.toString(16)
+    let hashx = bitcoin.crypto.sha256(x).toString('hex')
+    let redeemScript = bitcoin.script.compile([
+      /* MAIN IF BRANCH */
+      bitcoin.opcodes.OP_IF,
+      bitcoin.opcodes.OP_SHA256,
+      Buffer.from(hashx, 'hex'),
+      bitcoin.opcodes.OP_EQUALVERIFY,
+      bitcoin.opcodes.OP_DUP,
+      bitcoin.opcodes.OP_HASH160,
+
+      Buffer.from(destHash160, 'hex'), // wallet don't know storeman pubkey. //bitcoin.crypto.hash160(storeman.publicKey),//storeman.getPublicKeyBuffer(),// redeemer address
+      // bitcoin.crypto.hash160(storeman.publicKey),
+      bitcoin.opcodes.OP_ELSE,
+      bitcoin.script.number.encode(redeemblocknum),
+      bitcoin.opcodes.OP_CHECKLOCKTIMEVERIFY,
+      bitcoin.opcodes.OP_DROP,
+      bitcoin.opcodes.OP_DUP,
+      bitcoin.opcodes.OP_HASH160,
+
+      Buffer.from(revokerHash160, 'hex'),
+      // bitcoin.crypto.hash160(alice.publicKey),//alice.getPublicKeyBuffer(), // funder addr
+      /* ALMOST THE END. */
+      bitcoin.opcodes.OP_ENDIF,
+
+      // Complete the signature check.
+      bitcoin.opcodes.OP_EQUALVERIFY,
+      bitcoin.opcodes.OP_CHECKSIG
+    ])
+    console.log(redeemScript.toString('hex'))
+    // var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript));
+    // var address = bitcoin.address.fromOutputScript(scriptPubKey, network)
+
+    let addressPay = bitcoin.payments.p2sh({ redeem: { output: redeemScript, network: bitcoin.networks.testnet }, network: bitcoin.networks.testnet })
+    let address = addressPay.address
+
+    return {
+      'p2sh': address,
+      'x': x,
+      'hashx': hashx,
+      'redeemblocknum': redeemblocknum,
+      'redeemScript': redeemScript
+    }
+  },
+
+  getUTXOSBalance (utxos) {
+    let sum = 0
+    let i = 0
+    for (i = 0; i < utxos.length; i++) {
+      sum += utxos[i].amount
+    }
+    console.log('utxo balance=' + sum)
+
+    return sum
+  },
+
+  keysort (key, sortType) {
+    return function (a, b) {
+      return sortType ? ~~(a[key] < b[key]) : ~~(a[key] > b[key])
+    }
+  },
+
+  getTxSize (vin, vout) {
+    return vin * 180 + vout * 34 + 10 + vin
+  },
+
+  coinselect (utxos, target, feeRate) {
+    let ninputs = 0
+    let availableSat = 0
+    let inputs = []
+    let outputs = []
+    let fee = 0
+
+    utxos = utxos.sort(this.keysort('value', true))
+
+    for (let i = 0; i < utxos.length; i++) {
+      const utxo = utxos[i]
+      if (utxo.confirmations >= 3) {
+        availableSat += Math.round(utxo.value)
+        ninputs++
+        inputs.push(utxo)
+        fee = this.getTxSize(ninputs, 2) * feeRate
+        if (availableSat >= target.value + fee) {
+          break
+        }
+      }
+    }
+
+    if (availableSat < target.value) {
+      return new Error('You do not have enough in your wallet')
+    }
+
+    fee = this.getTxSize(ninputs, 2) * feeRate
+    let change = availableSat - target.value - fee
+
+    if (fee > target.value) {
+      return new Error('target value must be larger than the fee')
+    }
+
+    outputs.push(target)
+    outputs.push({'value': change})
+
+    return {inputs, outputs, fee}
+  },
+
+  getAddress (keypair) {
+    let pkh = bitcoin.payments.p2pkh({pubkey: keypair.publicKey, network: bitcoin.networks.testnet})
+    return pkh.address
+  },
+
+  setKey (key) {
+    this.key = key
+    this.hashKey = this.getHashKey(this.key)
+  },
+
+  getHashKey (key) {
+    // return BigNumber.random().toString(16);
+
+    let kBuf = new Buffer(key.slice(2), 'hex')
+    //        let hashKey = '0x' + util.sha256(kBuf);
+    let h = createKeccakHash('keccak256')
+    h.update(kBuf)
+    let hashKey = '0x' + h.digest('hex')
+    console.log('input key:', key)
+    console.log('input hash key:', hashKey)
+    return hashKey
+  },
+
+  generatePrivateKey () {
+    let randomBuf
+    do {
+      randomBuf = crypto.randomBytes(32)
+    } while (!secp256k1.privateKeyVerify(randomBuf))
+    return '0x' + randomBuf.toString('hex')
+  },
+
+  // user call this to lock btc
+  async btc2wbtcLock (keyPairArray, amount, feeRate, destHash160) {
+    let XX = this.generatePrivateKey()
+    let hashX = this.getHashKey(XX)
+
+    // generate script and p2sh address
+    let revokerHash160 = bitcoin.crypto.hash160(keyPairArray[0].publicKey)
+
+    let blocknum = await this.getBlockNumber(this.btcSender)
+    let contract = await this.hashtimelockcontract(hashX, blocknum + LOCK_BLK_NUMBER * 2, destHash160, revokerHash160)
+
+    // record it in map
+    let txId = await this.lock(contract, amount, keyPairArray, feeRate)
+
+    if (txId != undefined) {
+      // record it in map
+      contractsMap[txId] = contract
+      XXMap[txId] = XX
+      return {txId: txId, hashX: hashX, redeemblocknum: contract['redeemblocknum']}
+    } else {
+      return null
+    }
+  },
+
+  async wbtc2btcLock (keyPairArray, amount, feeRate, destPub) {
+    let XX = this.generatePrivateKey()
+    let hashX = this.getHashKey(XX)
+
+    // generate script and p2sh address
+    let contract = await this.hashtimelockcontract(hashX, LOCK_BLK_NUMBER, destPub, keyPairArray[0].pubkey)
+
+    let txId = await this.lock(contract, amount, keyPairArray, feeRate)
+
+    if (txId != undefined) {
+      // record it in map
+      contractsMap[txId] = contract
+      XXMap[txId] = XX
+      return {txId: txId, hashX: hashX, redeemblocknum: contract['redeemblocknum']}
+    } else {
+      return null
+    }
+  },
+
+  async btc2wbtcRefund (txHash, refunderKeyPair) {
+    let rawTx = getTxInfo(txHash)
+    let XX = XXMap[txHash]
+
+    return this.refund(rawTx, XX, refunderKeyPair)
+  },
+
+  async wbtc2btcrefund (txHash, refunderKeyPair) {
+    let XX = XXMap[txHash]
+    let rawTx = getTxInfo(txHash)
+    return this.refund(rawTx, XX, refunderKeyPair)
+  },
+
+  async btc2wbtcRevoke (txHash, revokerKeyPair) {
+    let hashX = this.getHashKey(XXMap[txHash])
+    let rawTx = getTxInfo(txHash)
+
+    return await this.revoke(rawTx, revokerKeyPair)
+  },
+
+  async wbtc2btcRevoke (txHash, revokerKeyPair) {
+    let rawTx = getTxInfo(txHash)
+    return await this.revoke(rawTx, revokerKeyPair)
+  },
+
+  async btcTxBuildSend (keyPairArray, amount, targets, feeRate) {
+    let addressArray = []
+    let addressKeyMap = {}
+
+    let i
+    for (i = 0; i < keyPairArray.length; i++) {
+      let kp = keyPairArray[i]
+      let address = this.getAddress(kp)
+      addressArray.push(address)
+      addressKeyMap[address] = kp
+    }
+
+    let utxos = await this.getBtcUtxo(this.btcSender, MIN_CONFIRM_BLKS, MAX_CONFIRM_BLKS, addressArray)
+
+    let balance = this.getUTXOSBalance(utxos)
+
+    if (balance <= amount) {
+      return {'result': null, 'error': new Error('utxo balance is not enough')}
+    }
+
+    utxos = utxos.map(function (item, index) {
+      item.value = item.amount * 100000000
+      return item
+    })
+
+    let {inputs, outputs, fee} = this.coinselect(utxos, targets, feeRate)
+
+    // .inputs and .outputs will be undefined if no solution was found
+    if (!inputs || !outputs) {
+      return {'result': null, 'error': new Error('utxo balance is not enough')}
+    }
+
+    console.log('fee', fee)
+
+    let txb = new bitcoin.TransactionBuilder(network)
+
+    for (i = 0; i < inputs.length; i++) {
+      let inItem = inputs[i]
+      txb.addInput(inItem.txid, inItem.vout)
+    }
+
+    // put out at 0 position
+    for (i = 0; i < outputs.length; i++) {
+      let outItem = outputs[i]
+      if (!outItem.address) {
+        txb.addOutput(addressArray[0], Math.round(outItem.value))
+      } else {
+        txb.addOutput(outItem.address,  Math.round(outItem.value))
+      }
+    }
+
+    for (i = 0; i < inputs.length; i++) {
+      let inItem = inputs[i]
+      let from = inItem.address
+      let signer = addressKeyMap[from]
+      txb.sign(i, signer)
+    }
+
+    const rawTx = txb.build().toHex()
+    console.log('rawTx: ', rawTx)
+
+    let result = await this.sendRawTransaction(this.btcSender, rawTx)
+    console.log('result hash:', result)
+
+    return {result: result, fee: fee}
+  },
+
+  async lock (contract, amount, keyPairArray, feeRate) {
+    // define target utxo
+    let targets = [
+      {
+        address: contract['p2sh'],
+        value: amount * 100000000
+      }
+    ]
+
+    return await this.btcTxBuildSend(keyPairArray, amount, targets, feeRate).result
+  },
+
+  // call this function to refund locked btc
+  async refund (fundtx, XX, refunderKeyPair) {
+    // get the contract from buffer
+    let contract = contractsMap[fundtx.vout.address]
+    if (contract == undefined) {
+      return null
+    }
+
+    let redeemScript = contract['redeemScript']
+
+    let txb = new bitcoin.TransactionBuilder(network)
+    txb.setVersion(1)
+
+    print4debug('----W----A----N----C----H----A----I----N----')
+    print4debug(JSON.stringify(fundtx))
+    print4debug('----W----A----N----C----H----A----I----N----')
+
+    txb.addInput(fundtx.txid, fundtx.vout)
+    txb.addOutput(this.getAddress(refunderKeyPair), (fundtx.amount - FEE) * 100000000)
+
+    let tx = txb.buildIncomplete()
+    let sigHash = tx.hashForSignature(0, redeemScript, bitcoin.Transaction.SIGHASH_ALL)
+
+    let redeemScriptSig = bitcoin.payments.p2sh({
+      redeem: {
+        input: bitcoin.script.compile([
+          bitcoin.script.signature.encode(refunderKeyPair.sign(sigHash), bitcoin.Transaction.SIGHASH_ALL),
+          refunderKeyPair.pubkey,
+          Buffer.from(XX, 'utf-8'),
+          bitcoin.opcodes.OP_TRUE
+        ]),
+        output: redeemScript
+      },
+      network: network
+    }).input
+
+    tx.setInputScript(0, redeemScriptSig)
+
+    let rawTx = tx.toHex()
+    print4debug('redeem raw tx: \n' + rawTx)
+    let result = await this.sendRawTransaction(this.btcSender, rawTx)
+    console.log('result hash:', result)
+
+    delete contractsMap[fundtx.vout.address]
+
+    return {'result': result, 'error': null}
+  },
+
+  // call this function to revoke locked btc
+  async revoke (fundtx, revokerKeyPair) {
+    let contract = contractsMap[fundtx.vout.address]
+    if (contract == undefined) {
+      return null
+    }
+
+    let redeemScript = contract['redeemScript']
+
+    let txb = new bitcoin.TransactionBuilder(network)
+    txb.setLockTime(contract['redeemblocknum'])
+    txb.setVersion(1)
+    txb.addInput(fundtx.txid, fundtx.vout, 0)
+    txb.addOutput(this.getAddress(revokerKeyPair), (fundtx.amount - FEE) * 100000000)
+
+    let tx = txb.buildIncomplete()
+    let sigHash = tx.hashForSignature(0, redeemScript, bitcoin.Transaction.SIGHASH_ALL)
+
+    let redeemScriptSig = bitcoin.payments.p2sh({
+      redeem: {
+        input: bitcoin.script.compile([
+          bitcoin.script.signature.encode(revokerKeyPair.sign(sigHash), bitcoin.Transaction.SIGHASH_ALL),
+          revokerKeyPair.publicKey,
+          bitcoin.opcodes.OP_FALSE
+        ]),
+        output: redeemScript
+      }
+    }).input
+
+    tx.setInputScript(0, redeemScriptSig)
+
+    let rawTx = tx.toHex()
+    print4debug('redeem raw tx: \n' + rawTx)
+    let result = await this.sendRawTransaction(this.btcSender, rawTx)
+    console.log('result hash:', result)
+
+    delete contractsMap[fundtx.vout.address]
+    return result
+  },
+
+  async btcSendTransaction (keyPairArray, amount, destAddress, feeRate) {
+    let target = {
+      address: destAddress,
+      value: Math.round(amount * 100000000)
+    }
+
+    return await this.btcTxBuildSend(keyPairArray, amount, target, feeRate)
+  }
 }
 
 exports.Backend = Backend;
--- tests/HTLCBTC.js
+++ tests/HTLCBTC.js
@@ -1,52 +1,64 @@
-'use strict';
+'use strict'
 
-const WanchainCore = require('../walletCore.js');
-const bitcoin  = require('bitcoinjs-lib');
-const Client = require('bitcoin-core');
-const config = require('../config.js');
-const pu = require('promisefy-util');
-const assert = require('chai').assert;
-const Htlc = require('../wanchaintrans/btc_cross_transactions/btcHTLCTransaction');
+const WanchainCore = require('../walletCore.js')
+const bitcoin = require('bitcoinjs-lib')
+const Client = require('bitcoin-core')
+const config = require('../config.js')
+const pu = require('promisefy-util')
+const assert = require('chai').assert
+const ccu= require('../ccUtil.js')
 
-var alice = bitcoin.ECPair.fromWIF(
-    'cPbcvQW16faWQyAJD5sJ67acMtniFyodhvCZ4bqUnKyjataXKLd5', bitcoin.networks.testnet
-);
-var bob = bitcoin.ECPair.fromWIF(
-    'cTgR2GrYrH3Z9UULxGEpfAhM4mAsmFRAULUwp5KYFM9R9khxYJ4v', bitcoin.networks.testnet
-);
+// var alice = bitcoin.ECPair.fromWIF(
+//     'cPbcvQW16faWQyAJD5sJ67acMtniFyodhvCZ4bqUnKyjataXKLd5', bitcoin.networks.testnet
+// );
+// var alice = bitcoin.ECPair.fromWIF(
+//     'cTgR2GrYrH3Z9UULxGEpfAhM4mAsmFRAULUwp5KYFM9R9khxYJ4v', bitcoin.networks.testnet
+// );
 
-const btcserver={
-    regtest:{
-        network: 'regtest',
-        host: "18.237.186.227",
-        port: 18443,
-        username: "USER",
-        password: "PASS"
-    }
-};
+var alice
 
-const client = new Client(btcserver.regtest);
+const btcserver = {
+  regtest: {
+    network: 'regtest',
+    host: '18.237.186.227',
+    port: 18443,
+    username: 'USER',
+    password: 'PASS'
+  }
+}
 
-var wanchainCore;
-var ccUtil;
-var btcUtil;
-var htlc;
+const client = new Client(btcserver.regtest)
 
-describe('btc api test', ()=> {
+var wanchainCore
+var ccUtil
+var btcUtil
 
-    before(async () => {
-        wanchainCore = new WanchainCore(config);
+describe('btc api test', () => {
+  var aliceAddr
+  var bobAddress
 
-        ccUtil = wanchainCore.be;
-        btcUtil = wanchainCore.btcUtil;
+  before(async () => {
+    wanchainCore = new WanchainCore(config)
 
-        await wanchainCore.init(config);
-        console.log("start");
-        htlc = new Htlc();
-        htlc.init();
+    ccUtil = wanchainCore.be
+    btcUtil = wanchainCore.btcUtil
 
-        let aliceAddr = htlc.getAddress(alice);
-        let txid = await client.sendToAddress(aliceAddr, 0.1);
+    await wanchainCore.init(config)
+    console.log('start')
+
+    function rng () { return Buffer.from('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz') }
+    alice = bitcoin.ECPair.makeRandom({ rng: rng, network: bitcoin.networks.testnet})
+    aliceAddr = ccUtil.getAddress(alice)
+    await client.importAddress(aliceAddr, '')
+
+    function rngBob () { return Buffer.from('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzr') }
+    var bob = bitcoin.ECPair.makeRandom({ rng: rngBob, network: bitcoin.networks.testnet})
+    bobAddress = ccUtil.getAddress(bob)
+    await client.importAddress(bobAddress, '')
+
+    //  assert.strictEqual(address, '1F5VhMHukdnUES9kfXqzPzMeF1GPHKiF64')
+
+    /*        let txid = await client.sendToAddress(aliceAddr, 0.1);
         console.log(txid);
 
         txid = await client.sendToAddress(aliceAddr, 0.12);
@@ -63,24 +75,57 @@ describe('btc api test', ()=> {
 
         txid = await client.sendToAddress(aliceAddr, 0.16);
         console.log(txid);
+*/
+  })
 
-        let aliceBalance = await client.getReceivedByAddress(aliceAddr, 0);
-        let bobBalance = await client.getReceivedByAddress(htlc.getAddress(bob), 0);
+  function sleep (milliSeconds) {
+    var startTime = new Date().getTime()
+    while (new Date().getTime() < startTime + milliSeconds);
+  };
 
-        console.log(" alice Balance: " + aliceBalance);
-        console.log(" bob   Balance: " + bobBalance);
+  it('TC000: test send normal transaction', async () => {
 
-    });
+    let utxos = await ccUtil.getBtcUtxo(ccUtil.btcSender,  1, 1000,  [aliceAddr])
+    let aliceBalance = ccUtil.getUTXOSBalance(utxos)
 
-    let txid;
+    utxos = await ccUtil.getBtcUtxo(ccUtil.btcSender, 1, 1000,  [bobAddress])
+    let bobBalance = ccUtil.getUTXOSBalance(utxos)
 
-    it('TC001: test htlcbtc lock', async ()=>{
-        txid = await htlc.btc2wbtcLock([alice],0.3,55,bob.publicKey);
-    });
+    console.log(' alice Balance: ' + aliceBalance)
+    console.log(' bob   Balance: ' + bobBalance)
 
-    it('TC002: test htlcbtc refund', async ()=>{
-        txid = await htlc.btc2wbtcRefund(txid.txId,[alice]);
-    });
+    let amount = 1
+    // let res = '';
+    let res = await ccUtil.btcSendTransaction([alice], amount, bobAddress, 55)
+    sleep(20 * 1000)
+
+    if (res.error != undefined) {
+      assert.equal(false, true, 'error send transaction')
+    }
+
+    console.log('txid=' + res.result)
+
+    utxos = await ccUtil.getBtcUtxo(ccUtil.btcSender, 1, 1000, [aliceAddr])
+    let afteraliceBalance = ccUtil.getUTXOSBalance(utxos)
+
+    utxos = await ccUtil.getBtcUtxo(ccUtil.btcSender, 1, 1000, [bobAddress])
+    let afterbobBalance = ccUtil.getUTXOSBalance(utxos)
 
 
+    console.log('after alice Balance: ' + afteraliceBalance)
+    console.log('after bob   Balance: ' + afterbobBalance)
+
+    assert.equal((aliceBalance - amount - (res.fee/100000000)).toFixed(8), afteraliceBalance.toFixed(8), 'alice balance is wrong')
+    assert.equal((bobBalance + amount).toFixed(8), afterbobBalance.toFixed(8), 'bob balance is wrong')
+
+  })
+
+
+  it('TC001: test htlcbtc lock', async () => {
+    // txid = await ccUtil.btc2wbtcLock([alice],0.3,55,bob.publicKey);
+  })
+
+  it('TC002: test htlcbtc refund', async () => {
+    // txid = await ccUtil.btc2wbtcRefund(txid.txId,[alice]);
+  })
 })
