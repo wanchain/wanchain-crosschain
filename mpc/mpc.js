ed348e2f19d89c20b349b7cd9bd4d544786b7008
Zhang Lihua <zhang.lihua@wanglutech.com>
2018-09-21 13:28:52 +0800

    update mpc

--- mpc/mpc.js
+++ mpc/mpc.js
@@ -16,7 +16,7 @@ function signMpcBtcTransaction(tx) {
         bitcoin.opcodes.OP_EQUALVERIFY,
         bitcoin.opcodes.OP_CHECKSIG
     ]).toString('hex');
-    config.log("signMpcBtcTransaction tx:", tx);
+    console.log("signMpcBtcTransaction tx:", tx);
     let mpcTx = {};
     mpcTx.Version = tx.version;
     mpcTx.LockTime = tx.locktime;
@@ -42,7 +42,7 @@ function signMpcBtcTransaction(tx) {
         o.PubKeyScrip = storemanScriptSig;
         mpcTx.TxIn.push(o);
     }
-    config.log("signMpcBtcTransaction mpcTx:", mpcTx);
+    console.log("signMpcBtcTransaction mpcTx:", mpcTx);
     return new Promise((resolve, reject) => {
         try {
             web3.storeman.signMpcBtcTransaction(mpcTx, (err, result) => {
